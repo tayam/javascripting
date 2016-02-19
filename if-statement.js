002b1af9cebb0ab3447ab82a5ff19d5db52ba45e
tayam <tayam@comcast.net>
2016-02-19 15:53:29 -0800

    solve if-statement.js

--- if-statement.js
+++ if-statement.js
@@ -16,3 +16,9 @@
      javascripting verify if-statement.js
 */
 
+var fruit = "orange"
+if (fruit.length > 5) {
+console.log("The fruit name has more than five characters.");
+} else {
+console.log("The fruit name has five characters or less.");
+}
